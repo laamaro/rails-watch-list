198903f1545be4613cb03197a71a4b4525e2a97d
Laura Amaro <lauraamaro1804@gmail.com>
2022-03-02 12:05:12 -0300

    cards

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,7 +15,6 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
 
     if @list.save
       redirect_to lists_path(@list)
@@ -27,6 +26,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
